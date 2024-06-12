c900d2722615e50f02bc2267fa9fdbdf9981626c
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-06-12 18:20:56 +0100

    Updated SocialLinks

--- src/SocialLinks.js
+++ src/SocialLinks.js
@@ -7,17 +7,11 @@ export default function SocialLinks() {
         Sophie Davies
       </a>
       , open sourced on {}
-      <a
-        href="https://github.com/SophieLMD/weather-forecast-app"
-        target="blank"
-      >
+      <a href="https://github.com/SophieLMD/weather-app-react" target="blank">
         Github {}
       </a>
       and website hosted on {}
-      <a
-        href="https://weather-forecasting-app-slmd.netlify.app/"
-        target="blank"
-      >
+      <a href="https://weather-app-react-slmd.netlify.app/" target="blank">
         Netlify
       </a>
     </footer>
